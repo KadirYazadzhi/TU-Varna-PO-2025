15daa43a86945f6337d299a08946bc6cb48bb8f9
Kadir Yazadzhi <smx590751@gmail.com>
2025-01-11 22:18:31 +0200

    Add Event Log Exercise

---
+++ 11.01/02. Event Log/02. Event Log/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    public static void Main(string[] args) {
+        int T = int.Parse(Console.ReadLine());
+        
+        for (int t = 0; t < T; t++) {
+            string input = Console.ReadLine();
+            Console.WriteLine(IsValid(input) ? "Valid" : "Invalid");
+        }
+    }
+    
+    private static bool IsValid(string s) {
+        int balance = 0;
+
+        foreach (char ch in s) {
+            if (ch == '(') {
+                balance++;
+            }
+            else if (ch == ')') {
+                balance--;
+                if (balance < 0) return false; 
+            }
+        }
+
+        return balance == 0; 
+    }
+}
