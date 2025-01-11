143f6e0dd95f4d26e4a630b90161fa7e1558b049
Kadir Yazadzhi <smx590751@gmail.com>
2025-01-11 22:21:22 +0200

    Add Barrels and Vines Exercise

---
+++ 11.01/03. Barrels and Vines/03. Barrels and Vines/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    public static void Main(string[] args) {
+        int T = int.Parse(Console.ReadLine()); 
+
+        for (int t = 0; t < T; t++) {
+            int N = int.Parse(Console.ReadLine());
+            
+            int oneThird = N / 3;  
+            int half = N / 2;      
+
+            int totalBottles = (oneThird * 2) + (half * 1);  
+            
+            if (totalBottles % 2 != 0) {
+                totalBottles -= 1;
+            }
+            
+            int remainingBottles = totalBottles / 2;
+
+            Console.WriteLine(N + remainingBottles); 
+        }
+    }
+}
