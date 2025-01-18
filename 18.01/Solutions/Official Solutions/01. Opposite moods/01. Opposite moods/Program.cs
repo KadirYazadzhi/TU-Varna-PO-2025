b0d977cb1e9f0828892631a395af1ce644bd8710
Kadir Yazadzhi <smx590751@gmail.com>
2025-01-18 12:51:51 +0200

    Add Opposite Moods Exercise

---
+++ 18.01/Solutions/Official Solutions/01. Opposite moods/01. Opposite moods/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());  
+
+        for (int i = 0; i < T; i++) {
+            string[] input = Console.ReadLine().Split();
+            string A = input[0]; 
+            string B = input[1];
+
+            bool isAPositive = A[0] != '-'; 
+            bool isBPositive = B[0] != '-'; 
+
+            if (isAPositive != isBPositive) {
+                Console.WriteLine("Opposite");
+            } 
+            else {
+                Console.WriteLine("Same");
+            }
+        }
+    }
+}
