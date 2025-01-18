3e6f0dc627903eab00a3a5689c1424f92ab934d8
Kadir Yazadzhi <smx590751@gmail.com>
2025-01-18 12:53:12 +0200

    Add Daily Status Exercise

---
+++ 18.01/Solutions/Official Solutions/04. Daily status/04. Daily status/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < T; i++) {
+            long N = long.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+            
+            if (D < 1 || D > 31) continue;
+            
+            string binaryRepresentation = Convert.ToString(N, 2);
+            
+            binaryRepresentation = binaryRepresentation.PadLeft(31, '0');
+            
+            if (binaryRepresentation[31 - D] == '1') Console.WriteLine("Operational");
+            else Console.WriteLine("Off");
+        }
+    }
+}
