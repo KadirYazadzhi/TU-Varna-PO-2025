99aab4f619442cfd851a2b8595eaa6881180f9ab
Kadir Yazadzhi <smx590751@gmail.com>
2025-01-28 00:06:19 +0200

    Add Archiving Exercise

---
+++ 25.01/Solutions/Official Solutions/03. Archiving/03. Archiving/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());
+
+        for (int t = 0; t < T; t++) {
+            string[] firstLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            int N = int.Parse(firstLine[0]);
+            int K = int.Parse(firstLine[1]);
+            
+            int[] fileSizes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            
+            var smallestFiles = fileSizes.OrderBy(size => size).Take(K);
+            
+            Console.WriteLine(string.Join(" ", smallestFiles));
+        }
+    }
+}
