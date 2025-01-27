5e1bf81bc75bda7b839b88868a48def7ad5c6449
Kadir Yazadzhi <smx590751@gmail.com>
2025-01-28 00:05:06 +0200

    Add Ritual Circle Exercise

---
+++ 25.01/Solutions/Official Solutions/01. Ritual Circle/01. Ritual Circle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());
+        
+        int[] inputs = new int[T];
+        for (int i = 0; i < T; i++) {
+            inputs[i] = int.Parse(Console.ReadLine());
+        }
+
+
+        foreach (int N in inputs) {
+            Console.WriteLine(CircularPermutations(N));
+        }
+    }
+
+    static BigInteger CircularPermutations(int N) {
+        if (N == 1) return 1; 
+
+        return Factorial(N - 1);
+    }
+
+    static BigInteger Factorial(int n) {
+        BigInteger result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
