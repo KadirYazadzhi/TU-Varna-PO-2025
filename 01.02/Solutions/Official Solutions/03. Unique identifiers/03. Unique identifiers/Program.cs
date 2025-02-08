1a353e4493ca7ff3090ad04ebd3e56b579885ad3
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-08 13:21:53 +0200

    Add Unique Identifiers exercise

---
+++ 01.02/Solutions/Official Solutions/03. Unique identifiers/03. Unique identifiers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine()); 
+
+        for (int t = 0; t < T; t++) {
+            int N = int.Parse(Console.ReadLine()); 
+            Dictionary<string, int> usernameToId = new Dictionary<string, int>();
+            Dictionary<int, string> idToUsername = new Dictionary<int, string>();
+            int uniqueId = 0;
+            
+            for (int i = 0; i < N; i++) {
+                string username = Console.ReadLine();
+                if (!usernameToId.ContainsKey(username)) {
+                    usernameToId[username] = uniqueId;
+                    idToUsername[uniqueId] = username;
+                    uniqueId++;
+                }
+            }
+
+            int Q = int.Parse(Console.ReadLine()); 
+            
+            for (int q = 0; q < Q; q++) {
+                string query = Console.ReadLine();
+                if (int.TryParse(query, out int id)) {
+                    if (idToUsername.ContainsKey(id)) {
+                        Console.WriteLine(idToUsername[id]);
+                    }
+                }
+                else {
+                    if (usernameToId.ContainsKey(query)) {
+                        Console.WriteLine(usernameToId[query]);
+                    }
+                }
+            }
+        }
+    }
+}
