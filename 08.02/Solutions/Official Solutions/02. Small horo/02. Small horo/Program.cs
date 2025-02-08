e59f0bbc9bca3cb2adb89c539e454478e9b927ac
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-08 13:24:14 +0200

    Add Small horo exercise

---
+++ 08.02/Solutions/Official Solutions/02. Small horo/02. Small horo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());
+        
+        while (T-- > 0) {
+            string s = Console.ReadLine();
+            Console.WriteLine(CountValidSplits(s));
+        }
+    }
+
+    static int CountValidSplits(string s) {
+        int n = s.Length;
+        int[] balance = new int[n * 2 + 1];
+        int minBalance = 0, count = 0;
+
+ 
+        for (int i = 0; i < n; i++) {
+            balance[i + 1] = balance[i] + (s[i] == '(' ? 1 : -1);
+            minBalance = Math.Min(minBalance, balance[i + 1]);
+        }
+
+
+        if (balance[n] != 0) return 0;
+
+        for (int i = 0; i < n; i++) {
+            if (balance[i] == minBalance) count++;
+        }
+
+        return count;
+    }
+}
