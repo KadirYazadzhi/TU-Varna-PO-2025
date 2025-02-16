6fb4d785ab10c9e13d1993a72b4ec64457a29cda
Kadir Yazadzhi <smx590751@gmail.com>
2025-02-16 12:10:04 +0200

    Add Lighting Control Exercise

---
+++ 15.02/Solutions/Official Solutions/03. Lighting Control/03. Lighting Control/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());
+        
+        for (int t = 0; t < T; t++) {
+            int N = int.Parse(Console.ReadLine());
+            int Q = int.Parse(Console.ReadLine());
+            
+            for (int q = 0; q < Q; q++) {
+                int P = int.Parse(Console.ReadLine());
+                
+                N ^= (1 << (P - 1));
+            }
+            
+            Console.WriteLine(N);
+        }
+    }
+}
