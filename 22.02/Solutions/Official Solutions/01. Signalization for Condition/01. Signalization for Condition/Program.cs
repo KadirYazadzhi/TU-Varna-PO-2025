36efc831e8c28735f45a2cfad6a4f6610ad07bd4
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-03-04 10:41:03 +0200

    Add Signalization for Condition Exercise

---
+++ 22.02/Solutions/Official Solutions/01. Signalization for Condition/01. Signalization for Condition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < T; i++){
+            string[] input = Console.ReadLine().Split();
+            int X = int.Parse(input[0]);
+            int n = int.Parse(input[1]);
+            
+            int result = X | (1 << n);
+            Console.WriteLine(result);
+        }
+    }
+}
