33a36744a5cb3cbc8e02b7b3571c526c0e66af37
Kadir Yazadzhi <160729048+KadirYazadzhi@users.noreply.github.com>
2025-03-30 04:22:08 +0300

    Add State Changes Exercise

---
+++ On-Site Round - 30.03/04. State Change/Solution/04. State Change/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+        int T = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < T; i++) {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int X = int.Parse(input[0]);
+            int N = int.Parse(input[1]);
+            
+            int result = X ^ (1 << N);
+            Console.WriteLine(result);
+        }
+    }
+}
